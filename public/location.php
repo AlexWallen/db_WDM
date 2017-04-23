7b20e036616cac8d315c1d7936ddb056020b01e9
Alexander Wallen <Alex_Wallen@baylor.edu>
2017-04-22 19:41:32 -0500

    API for Location done

--- public/location.php
+++ public/location.php
@@ -9,4 +9,36 @@
         }
         echo json_encode($data);
     });
+
+    $app->post('/location/add', function($request){
+        require_once('db.php');
+        $query = "INSERT INTO Location (latitude, longitude, state, county) VALUES (?,?,?,?)";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("ddss",$lat,$long,$state,$county);
+        $lat = $request->getParsedBody()['latitude'];
+        $long = $request->getParsedBody()['longitude'];
+        $state = $request->getParsedBody()['state'];
+        $county = $request->getParsedBody()['county'];
+        $stmt->execute();
+    });
+
+    $app->put('/location/edit/{location_id}', function($request){
+        require_once('db.php');
+        $get_id = $request->getAttribute('location_id');
+        $query = "UPDATE lOCATION SET latitude = ?, longitude = ?, state = ?, county = ? WHERE id = $get_id";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("ddss",$lat,$long,$state,$county);
+        $lat = $request->getParsedBody()['latitude'];
+        $long = $request->getParsedBody()['longitude'];
+        $state = $request->getParsedBody()['state'];
+        $county = $request->getParsedBody()['county'];
+        $stmt->execute();
+    });
+
+    $app->delete('/location/delete/{location_id}', function($request){
+        require_once('db.php');
+        $get_id = $request->getAttribute('location_id');
+        $query = "DELETE from Location WHERE id = $get_id";
+        $result = $conn->query($query);
+    });
 ?>

49c0d58c41b6506be8943191ab8a259b9bb133ca
EanEvans <Ean_Evans@baylor.edu>
2017-04-24 09:53:17 -0500

    Rainfall

--- public/rainfall.php
+++ public/rainfall.php
@@ -9,4 +9,34 @@
         }
         echo json_encode($data);
     });
+    $app->post('/rainfall/add', function($request){
+        require_once('db.php');
+        $query = "INSERT INTO Rainfall (location_id, amount, normal_amount, rtime) VALUES (?,?,?,?)";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("idds",$location_id,$amount,$normal_amount,$rtime);
+        $location_id = $request->getParsedBody()['location_id'];
+        $amount = $request->getParsedBody()['amount'];
+        $normal_amount = $request->getParsedBody()['normal_amount'];
+        $rtime = $request->getParsedBody()['rtime'];
+        $stmt->execute();
+    });
+
+    /*$app->put('/rainfall/edit/{rainid}', function($request){
+        require_once('db.php');
+        $get_id = $request->getAttribute('location_id');
+        $query = "UPDATE Rainfall SET amount = ?, normal_amount = ? time = ? WHERE id = $get_id";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("ss",$name,$type);
+        $amount = $request->getParsedBody()['amount'];
+        $normal_amount = $request->getParsedBody()['normal_amount'];
+        $time = $request->getParsedBody()['time'];
+        $stmt->execute();
+    });
+
+    $app->delete('/rainfall/delete/{ownerid}', function($request){
+        require_once('db.php');
+        $get_id = $request->getAttribute('location_id');
+        $query = "DELETE from Rainfall WHERE id = $get_id";
+        $result = $conn->query($query);
+    });*/
 ?>

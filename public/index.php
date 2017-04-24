013f559eff8a193093eec4c09ff27b31d7d6fa14
EanEvans <Ean_Evans@baylor.edu>
2017-04-24 09:55:38 -0500

    Rain

--- public/index.php
+++ public/index.php
@@ -5,22 +5,6 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
-<<<<<<< HEAD
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
-
-    return $response;
-});
-
-//Customer Routes
-
-//require '../src/routes/customers.php';
-
-
-$app->run();
-=======
-
 require 'wells.php';
 require 'transducers.php';
 require 'recordings.php';
@@ -35,23 +19,10 @@ require 'location.php';
     return $response;
 });
 $app->run();*/
-
+1
 $app->get('/', function() {
     echo '<strong>Welcome to the API</strong>';
 });
 
 $app->run();
-
 ?>
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> e383db9e3414dc615f86c58489a525a17f4dc87c
